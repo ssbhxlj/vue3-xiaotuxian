229bb9376293c5b4ac6a7084e7ba04a85fa967f9
ssbhxlj <2655501776@qq.com>
2025-03-22 23:44:40 +0800

    elementPlus按需引入配置

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -17,7 +17,8 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <RouterView /> -->
+  <el-button type="primary">Primary</el-button>
 </template>
 
 <style scoped>
