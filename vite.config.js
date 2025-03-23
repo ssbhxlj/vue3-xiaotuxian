097e6a129a3c34e46f62902390c10ca9752f58fe
ssbhxlj <2655501776@qq.com>
2025-03-23 10:37:19 +0800

    element主题色定制

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,83 @@
-import { fileURLToPath, URL } from 'node:url'
+// import { fileURLToPath, URL } from 'node:url'
 
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import vueDevTools from 'vite-plugin-vue-devtools'
+
+// // element-plus按需导入
+// import AutoImport from 'unplugin-auto-import/vite'
+// import Components from 'unplugin-vue-components/vite'
+// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//     // element-plus按需导入
+//     AutoImport({
+//       resolvers: [ElementPlusResolver()],
+//     }),
+//     Components({
+//       resolvers: [ElementPlusResolver({importStyle: 'sass'})],
+//     }),
+//     // // 按需定制主题配置
+//     // ElementPlus({
+//     //   useSource: true,
+//     // }),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//     css: {
+//       preprocessorOptions: {
+//         scss: {
+//           // 自动导入定制化样式文件进行样式覆盖
+//           additionalData: `
+//             @use "@/styles/element/index.scss" as *;
+//           `,
+//         }
+//       }
+//     },
+//   },
+// })
+
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// element-plus按需导入
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
-// https://vite.dev/config/
+// 导入对应包
+// import ElementPlus from 'unplugin-element-plus/vite'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
-    // element-plus按需导入
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({importStyle: 'sass'})],
     }),
+    // 按需定制主题配置，用不用都一样
+    // ElementPlus({
+    //   useSource: true,
+    // }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
