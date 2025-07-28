0514be91face27b83ffebf0798a6574825bb1286
zhangyaowen.37 <zhangyaowen.37@jd.com>
2025-07-28 10:58:24 +0800

    支付-渲染基础数据

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -1,5 +1,16 @@
 <script setup>
-const payInfo = {}
+import { getOrderAPI } from '@/apis/pay';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
+const payInfo = ref({});
+const getPayInfo = async () => {
+  const res = await getOrderAPI(route.query.orderId); // 改成 orderId
+  payInfo.value = res.result;
+};
+onMounted(() => {
+  getPayInfo();
+});
 </script>
 
 
